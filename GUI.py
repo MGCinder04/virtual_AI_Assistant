279eaaa93313db1148644408f9e73ba4bc7a15b9
mohit <mohitmkggupta@gmail.com>
2025-08-13 17:12:11 +0000

    completed gui

--- GUI.py
+++ GUI.py
@@ -7,6 +7,19 @@ root.geometry("550x675")
 root.resizable(False, False)
 root.config(bg="#6f8faf")
 
+
+def ask():
+    print("Ask")
+
+
+def send():
+    print("Send")
+
+
+def del_text():
+    print("deleting text...")
+
+
 frame = LabelFrame(root, padx=100, pady=7, borderwidth=3, relief="raised")
 frame.config(bg="#6f8faf")
 frame.grid(row=0, column=1, padx=55, pady=10)
@@ -24,6 +37,43 @@ text = Text(root, font=("courier 10 bold"), bg="#356696")
 text.grid(row=2, column=0)
 text.place(x=100, y=375, width=375, height=100)
 
+entry = Entry(root, justify=CENTER)
+entry.place(x=100, y=500, width=350, height=30)
 
+Button1 = Button(
+    root,
+    text="Ask",
+    bg="#356696",
+    pady=16,
+    padx=40,
+    borderwidth=3,
+    relief=SOLID,
+    command=ask,
+)
+Button1.place(x=70, y=575)
+
+Button2 = Button(
+    root,
+    text="Send",
+    bg="#356696",
+    pady=16,
+    padx=40,
+    borderwidth=3,
+    relief=SOLID,
+    command=send,
+)
+Button2.place(x=400, y=575)
+
+Button3 = Button(
+    root,
+    text="Delete",
+    bg="#356696",
+    pady=16,
+    padx=40,
+    borderwidth=3,
+    relief=SOLID,
+    command=del_text,
+)
+Button3.place(x=225, y=575)
 
 root.mainloop()
